762589c2c7086b2052b894f534c241e91bf55db2
ysys1195 <73346157+ysys1195@users.noreply.github.com>
2022-04-30 00:12:46 +0900

    カードの表示

--- src/App.js
+++ src/App.js
@@ -39,10 +39,7 @@ const App = () => {
           } else {
             setCards([newCard]);
           }
-          setCardId(cardId + 1)
-          setTitle('')
-          setBody('')
-          setCategory('')
+          setInputCard({ id: inputCard.id + 1, title: '', body: '', category: '' })
         }
       } else if (cardNum < cards.length) {
         console.log('↓これがdeleteCardId========');
@@ -55,80 +52,81 @@ const App = () => {
     }
     changeCard()
   }, [cardNum])
+  // カードの表示まで５０の途中
 
   return (
     <>
       <Box sx={{ borderBottom: '1px solid lightgray', paddingBottom: '50px' }}>
-       <h1>Post Action List</h1> カード枚数：{cardNum}
-       <form>
-         <TextField
-           className='input-form'
-           id='title'
-           label='Title'
-           variant='outlined'
-           value={inputCard.title}
-           onChange={e => setInputCard(
-             {
-               id: inputCard.id,
-               title: e.target.value,
-               body: inputCard.body,
-               category: inputCard.category
-             }
-           )}
-         />
-         <TextField
-           className='input-form'
-           id='body'
-           label='Body'
-           multiline
-           rows={4}
-           variant='outlined'
-           value={inputCard.body}
-           onChange={e => setInputCard(
-             {
-               id: inputCard.id,
-               title: inputCard.title,
-               body: e.target.value,
-               category: inputCard.category
-             }
-           )}
-         />
-         <TextField
-           className='input-form'
-           id='category'
-           label='Category'
-           variant='outlined'
-           value={inputCard.category}
-           onChange={e => setInputCard(
-             {
-               id: inputCard.id,
-               title: inputCard.title,
-               body: inputCard.body,
-               category: e.target.value
-             }
-           )}
-         />
-         <Button className='input-form' variant='contained' onClick={postAction} disabled={inputAllForm}>post</Button>
-       </form>
-     </Box>
+        <h1>Post Action List</h1> カード枚数：{cardNum}
+        <form>
+          <TextField
+            className='input-form'
+            id='title'
+            label='Title'
+            variant='outlined'
+            value={inputCard.title}
+            onChange={e => setInputCard(
+              {
+                id: inputCard.id,
+                title: e.target.value,
+                body: inputCard.body,
+                category: inputCard.category
+              }
+            )}
+          />
+          <TextField
+            className='input-form'
+            id='body'
+            label='Body'
+            multiline
+            rows={4}
+            variant='outlined'
+            value={inputCard.body}
+            onChange={e => setInputCard(
+              {
+                id: inputCard.id,
+                title: inputCard.title,
+                body: e.target.value,
+                category: inputCard.category
+              }
+            )}
+          />
+          <TextField
+            className='input-form'
+            id='category'
+            label='Category'
+            variant='outlined'
+            value={inputCard.category}
+            onChange={e => setInputCard(
+              {
+                id: inputCard.id,
+                title: inputCard.title,
+                body: inputCard.body,
+                category: e.target.value
+              }
+            )}
+          />
+          <Button className='input-form' variant='contained' onClick={postAction} disabled={inputAllForm}>post</Button>
+        </form>
+      </Box>
 
       <Box>
         <h1>Action List</h1>
         {cards.map((card) => (
-          <Card sx={{ maxWidth: 500, margin: 2 }} key={card.cardId}>
+          <Card sx={{ maxWidth: 500, margin: 2 }} key={card.id}>
             <CardContent>
               <Typography sx={{ fontSize: 20, borderBottom: '1px solid lightgray' }}>
-                {card.cardTitle}
+                {card.title}
               </Typography>
               <Typography sx={{ fontSize: 16, padding: 2 }}>
-                {card.cardBody}
+                {card.body}
               </Typography>
               <Typography sx={{ fontSize: 12, padding: 2, color: '#0040a4' }}>
-                #{card.cardCategory}
+                #{card.category}
               </Typography>
             </CardContent>
             <CardActions>
-              <Button color='warning' variant='outlined' onClick={(e) => deleteCard(card.cardId, e)}>Delete</Button>
+              <Button color='warning' variant='outlined' onClick={(e) => deleteCard(card.id, e)}>Delete</Button>
             </CardActions>
           </Card>
         ))}
@@ -139,3 +137,4 @@ const App = () => {
 
 export default App
 
+137.56
